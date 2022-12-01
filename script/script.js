63ea73b8518784b3c15f4a55747aa449abc87823
GiorgioDellutri <giorgiodll93@gmail.com>
2022-12-01 17:06:57 +0100

    check explosion

--- script/script.js
+++ script/script.js
@@ -1,54 +1,72 @@
-// RECUPERO IL TASTO PLAY DALLA SUA CLASSE
+//! RECUPERO IL TASTO PLAY DALLA SUA CLASSE
 let play = document.querySelector('div.play');
 
-    // RECUPERO IL PARENT
+    //* RECUPERO IL PARENT
     const wrapperContainer = document.querySelector('div.grid-container');
-    // INIZIALIZZO LA GRIGLIA
+
+    //* INIZIALIZZO LA GRIGLIA
     wrapperContainer.innerHTML= "";
     
-// AGGIUNGO AL BOTTONE PLAY UN EVENTO. ESSO CONTERRA' TUTTO L'ALGORITMO
+//! AGGIUNGO AL BOTTONE PLAY UN EVENTO. ESSO CONTERRA' TUTTO L'ALGORITMO
 play.addEventListener('click', function(){
 
-    // CREO IL QUADRATO 100 VOLTE
+    //* CREO IL QUADRATO 100 VOLTE
     for (let i = 1; i <= 100; i++){
         const newSquare = squareCreation(i);
-        // AGGIUNGO IL DIV AL PARENT
+
+        //* AGGIUNGO IL DIV AL PARENT
         wrapperContainer.appendChild(newSquare);
     }
 }, {once : true});
 
-// BOMBE###########################
+
+//! BOMBS ###########################
 let bombs = [];
 
 for (let i = 0; i <= 16; i++){
-    // GENERO UN NUMERO RANDOMICO TRA 1 E 100
-    const randomBombIndex = randomNumber(1, 100);
+    //* GENERO UN NUMERO RANDOMICO TRA 1 E 100
+    let randomBombIndex = randomNumber(1, 100);
+
     // console.log(randomBombIndex);
-    // CONTROLLO SE IL NUMERO E' GIA' PRESENTE ALL'INTERNO DELL'ARRAY . SE NON E' PRESENTE LO AGGIUNGO
+    //* CONTROLLO SE IL NUMERO E' GIA' PRESENTE ALL'INTERNO DELL'ARRAY . SE NON E' PRESENTE LO AGGIUNGO
     if (! bombs.includes(randomBombIndex)){
         bombs.push(randomBombIndex)
     }
     console.log(bombs);
 }
 
-// FUNZIONE PER CREARE IL QUADRATO
+
+//! FUNZIONE PER CREARE IL QUADRATO
 function squareCreation (content){
-    // CREO IL DIV
+    
+    //* CREO IL DIV
     const newSquare = document.createElement('div');
-    // ATTRIBUISCO LE PROPRIETA CHE VOLUTE
+    
+    //* ATTRIBUISCO LE PROPRIETA CHE VOLUTE
     newSquare.classList.add('square', 'd-flex');
-    // AGGIUNGO IL NUMERO DELLA CASELLA
+    
+    //* AGGIUNGO IL NUMERO DELLA CASELLA
     newSquare.innerHTML = `<span class="m-auto"> ${content} </span>`;
-    // TRA CUI UN COMPORTAMENTO AL CLICK
+    
+    //* TRA CUI UN COMPORTAMENTO AL CLICK
     newSquare.addEventListener("click", function(){
-        // aggiungo la classe per colorarlo di azzurro
+        
+        //* AGGIUNGO LA CLASSE PER COLORARLO DI AZZURRO
         newSquare.classList.toggle('clicked');
         console.log(content);
-    });
+        
+        //! VERIFICA ESPLOSIONE
+        if (bombs.includes(content)){
+            newSquare.classList.replace('clicked', 'red');
+            window.alert('GAME OVER!');
+        }
 
+
+    });
     return newSquare;
 } 
-// FUNZIONE PER GENERARE NUMERI RANDOMICI
+
+//! FUNZIONE PER GENERARE NUMERI RANDOMICI
 function randomNumber (numMin , numMax){
     if (numMin === numMax){
         return numMax;
