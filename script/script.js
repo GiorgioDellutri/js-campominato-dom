057744c0c358978e1470f3fe48cb4e9aa65f3291
GiorgioDellutri <giorgiodll93@gmail.com>
2022-12-01 15:56:41 +0100

    console log square content (number)

---
+++ script/script.js
@@ -0,0 +1,45 @@
+// RECUPERO IL TASTO PLAY DALLA SUA CLASSE
+let play = document.querySelector('div.play');
+
+    // RECUPERO IL PARENT
+    const wrapperContainer = document.querySelector('div.grid-container');
+    // INIZIALIZZO LA GRIGLIA
+    wrapperContainer.innerHTML= "";
+    
+// AGGIUNGO AL BOTTONE PLAY UN EVENTO. ESSO CONTERRA' TUTTO L'ALGORITMO
+play.addEventListener('click', function(){
+
+    // CREO IL QUADRATO 100 VOLTE
+    for (let i = 1; i <= 100; i++){
+        const newSquare = squareCreation(i);
+        // AGGIUNGO IL DIV AL PARENT
+        wrapperContainer.appendChild(newSquare);
+    }
+}, {once : true});
+
+// FUNZIONE PER CREARE IL QUADRATO
+function squareCreation (content){
+    // CREO IL DIV
+    const newSquare = document.createElement('div');
+    // ATTRIBUISCO LE PROPRIETA CHE VOLUTE
+    newSquare.classList.add('square', 'd-flex');
+    // AGGIUNGO IL NUMERO DELLA CASELLA
+    newSquare.innerHTML = `<span class="m-auto"> ${content} </span>`;
+    // TRA CUI UN COMPORTAMENTO AL CLICK
+    newSquare.addEventListener("click", function(){
+        // aggiungo la classe per colorarlo di azzurro
+        newSquare.classList.toggle('clicked');
+        console.log(content);
+    });
+
+    return newSquare;
+} 
+
+function squareIndex (index){
+    for(let i = 1; i <= 100; i++){
+        console.log(index[i]);
+    }
+    return index;
+}
+
+
